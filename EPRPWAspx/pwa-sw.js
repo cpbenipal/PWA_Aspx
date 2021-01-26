923885c4927113705aec524aefd6db08597c0749
cpbenipal <cpbenipal@gmail.com>
2021-01-26 21:32:22 +0530

    Update pwa-sw.js

--- EPRPWAspx/pwa-sw.js
+++ EPRPWAspx/pwa-sw.js
@@ -1,47 +1,79 @@
-﻿// This is the "Offline page" service worker
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
-
-const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
-
-self.addEventListener("message", (event) => {
-    if (event.data && event.data.type === "SKIP_WAITING") {
-        self.skipWaiting();
-    }
-});
-
-self.addEventListener('install', async (event) => {
+ var PRECACHE_URLS = [
+    new Request('Employees.aspx'),
+    new Request('Rating.aspx'),
+    new Request('/assets/favicon.png') 
+]; 
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';      
+var version = 'v1::';
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener("install", function (event) {
+    console.log('WORKER: install event in progress.');
     event.waitUntil(
-        caches.open(CACHE)
-            .then((cache) => cache.add(offlineFallbackPage))
+        /* The caches built-in is a promise-based API that helps you cache responses,
+           as well as finding and deleting them.
+        */
+        caches
+            /* You can open a cache by name, and this method returns a promise. We use
+               a versioned cache name here so that we can remove old cache entries in
+               one fell swoop later, when phasing out an older service worker.
+            */
+            .open(version + 'fundamentals')
+            .then(function (cache) {
+                /* After the cache is opened, we can fill it with the offline fundamentals.
+                   The method below will add all resources we've indicated to the cache,
+                   after making HTTP requests for each of them.
+                */
+                return cache.addAll(PRECACHE_URLS);
+            })
+            .then(function () {
+                console.log('WORKER: install completed');
+            })
     );
 });
 
-if (workbox.navigationPreload.isSupported()) {
-    workbox.navigationPreload.enable();
-}
+self.addEventListener("activate", function (event) {
+    /* Just like with the install event, event.waitUntil blocks activate on a promise.
+       Activation will fail unless the promise is fulfilled.
+    */
+     
 
-self.addEventListener('fetch', (event) => {
-    if (event.request.mode === 'navigate') {
-        event.respondWith((async () => {
-            try {
-                const preloadResp = await event.preloadResponse;
-
-                if (preloadResp) {
-                    return preloadResp;
-                }
-
-                const networkResp = await fetch(event.request);
-                return networkResp;
-            } catch (error) {
-
-                const cache = await caches.open(CACHE);
-                const cachedResp = await cache.match(offlineFallbackPage);
-                return cachedResp;
-            }
-        })());
-    }
+    event.waitUntil(
+        caches
+            /* This method returns a promise which will resolve to an array of available
+               cache keys.
+            */
+            .keys()
+            .then(function (keys) {
+                // We return a promise that settles when all outdated caches are deleted.
+                return Promise.all(
+                    keys
+                        .filter(function (key) {
+                            // Filter by keys that don't start with the latest version prefix.
+                            return !key.startsWith(version);
+                        })
+                        .map(function (key) {
+                            /* Return a promise that's fulfilled
+                               when each outdated cache is deleted.
+                            */
+                            return caches.delete(key);
+                        })
+                );
+            })
+            .then(function () {
+                console.log('WORKER: activate completed.');
+            })
+    );
 });
+
+self.addEventListener('fetch', function (event) {        
+    event.respondWith(
+        caches.match(event.request, {
+            ignoreSearch: true
+        }).then(function (response) {            
+            return response || fetch(event.request);
+        })
+    );
+});
+
+       
